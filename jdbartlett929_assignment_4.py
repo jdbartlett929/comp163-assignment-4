7d3e17ee128f3a7b179b642d7040b28a9d653b99
jdbartlett929 <jdbartlett@aggies.ncat.edu>
2025-09-26 22:02:00 -0400

    Add Step 1 code: starting stats variables and prints

---
+++ jdbartlett929_assignment_4.py
@@ -0,0 +1,8 @@
+student_name = "Julian Bartlett"
+current_gpa = 4.0
+study_hours = 25
+social_points = 50
+stress_level = 40
+
+print("Welcome to College Life,", student_name)
+print("Starting stats -> GPA:", current_gpa, "Study:", study_hours, "Social:", social_points, "Stress:", stress_level)
